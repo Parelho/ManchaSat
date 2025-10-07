0b53b0dde34c65488c934ae095d8fe13380ecbc7
Vinicius Parelho <vinicius.parelho@outlook.com>
2025-10-07 15:43:06 -0300

    feat: oil spill can be tracked even while ais is not being sent

--- python/monitor_ships.py
+++ python/monitor_ships.py
@@ -6,11 +6,14 @@ import tensorflow as tf
 from image_processing import ImageProcessing as ip
 from ais import AIS
 import glob
+import random
 
 model = tf.keras.models.load_model("jupyter/segmentation_unet.h5", compile=False)
+all_ships = [] # Used to track ships that stopped sending AIS
 
 # ---------- GLOBAL PREVIOUS SPILL MASK ----------
 prev_oil_mask = None  # store mask of previous frame
+old_oil_spill = None  # store the position where spill first appeared
 
 def get_new_spill_mask(pred_mask):
     global prev_oil_mask
@@ -24,7 +27,20 @@ def get_new_spill_mask(pred_mask):
     prev_oil_mask = current_mask.copy()
     return new_spill
 
-def get_closest_ship(image):
+def calculate_closest_ship(ships, oil_centers):
+    closest_ship = ships[0]
+    diff = 0
+    min_diff = abs(oil_centers[0][0] - ships[0]["lon_px"]) + abs(oil_centers[0][1] - ships[0]["lat_px"])
+
+    for ship in ships[1:]:
+        diff = abs(oil_centers[0][0] - ship["lon_px"]) + abs(oil_centers[0][1] - ship["lat_px"])
+        if diff < min_diff:
+            closest_ship = ship
+            min_diff = diff
+    
+    return closest_ship, min_diff
+
+def get_closest_ship(image, ships_near_oil):
     # ---------- IMAGE ----------
     img_path = image
     frame = cv2.imread(img_path)
@@ -38,37 +54,62 @@ def get_closest_ship(image):
     centers, bboxes, cleaned_mask = ip.detect_ship_centers_from_mask(pred_mask, frame.shape)
 
     # ---------- NEW OIL SPILL REGION ----------
+    global old_oil_spill
     new_spill_mask = get_new_spill_mask(pred_mask)
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(new_spill_mask, connectivity=8)
 
     oil_centers = [(int(cx), int(cy)) for cx, cy in centroids[1:]]
 
-    if oil_centers:
+    if oil_centers and old_oil_spill is None:
+        old_oil_spill = new_spill_mask
+
+    if centers:
         ships = []
         for (cx, cy) in centers:
             nmea = AIS.simulate_ais(custom_coords=True, pixel_coord=(cx, cy))
             if AIS.checksum(nmea[0]):
                 msg = AIS.decode(nmea[0])
-                ships.append({
+                ship = {
                     "mmsi": msg.mmsi,
                     "lon": msg.lon,
                     "lat": msg.lat,
                     "lon_px": cx,
                     "lat_px": cy
-                })
+                }
+                ships.append(ship)
+                # Add new ship to all ships list or update the position
+                existing_ship = next((ship for ship in all_ships if ship["mmsi"] == msg.mmsi), None)
+                if existing_ship is None:
+                    all_ships.append(ship)
+                else:
+                    existing_ship.update(ship)
 
         closest_ship = None
-        if ships:
-            closest_ship = ships[0]
-            min_diff = abs(oil_centers[0][0] - ships[0]["lon_px"]) + abs(oil_centers[0][1] - ships[0]["lat_px"])
+        spiller_is_hidden = False
+        if ships and oil_centers:
+            if random.randint(0, 9) < 10:
+                closest_ship, _ = calculate_closest_ship(ships, oil_centers)
+                ships.remove(closest_ship) # Hides oil spiller
 
-            for ship in ships[1:]:
-                diff = abs(oil_centers[0][0] - ship["lon_px"]) + abs(oil_centers[0][1] - ship["lat_px"])
-                if diff < min_diff:
-                    closest_ship = ship
-                    min_diff = diff
+            closest_current_ship, current_diff = calculate_closest_ship(ships, oil_centers)
+            if old_oil_spill is not None:
+                num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(old_oil_spill, connectivity=8)
+                if len(centroids) > 1:
+                    old_oil_center = (int(centroids[1][0]), int(centroids[1][1]))
+                    closest_old_ship, old_diff = calculate_closest_ship(all_ships, [old_oil_center])
+                else:
+                    closest_old_ship, old_diff = calculate_closest_ship(all_ships, oil_centers)
+            else:
+                closest_old_ship, old_diff = calculate_closest_ship(all_ships, oil_centers)
 
-        return closest_ship
+            if current_diff < old_diff:
+                closest_ship = closest_current_ship
+            else:
+                closest_ship = closest_old_ship
+                spiller_is_hidden = True
+                old_oil_spill = new_spill_mask
+
+        return closest_ship, spiller_is_hidden
     else:
         return None
 
@@ -77,7 +118,7 @@ images_dir = "grayscale_frames"
 ships_near_oil = []
 
 for file_path in sorted(glob.glob(os.path.join(images_dir, "*.png"))):
-    closest_ship = get_closest_ship(file_path)
+    closest_ship, spiller_is_hidden = get_closest_ship(file_path, ships_near_oil)
 
     if closest_ship is not None:
         existing_ship = next((ship for ship in ships_near_oil if ship["mmsi"] == closest_ship["mmsi"]), None)
@@ -87,6 +128,10 @@ for file_path in sorted(glob.glob(os.path.join(images_dir, "*.png"))):
             print(f"New ship near oil: {closest_ship}")
         else:
             existing_ship["proximity_count"] += 1
-            print(f"Ship {closest_ship['mmsi']} has the highest proximity count at: {existing_ship['proximity_count']}")
+            print("The spilling ship is hiding it's AIS signal\n")
+
+if ships_near_oil:
+    top_ship = max(ships_near_oil, key=lambda s: s["proximity_count"])
+    print(f"Ship with highest proximity count: {top_ship['mmsi']} ({top_ship['proximity_count']})")
 
 print(ships_near_oil)
